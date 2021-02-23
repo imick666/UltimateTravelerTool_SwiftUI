2e23cf217cb5f8e225e3c185f58882d51fcc7f8e
Ruzel Mickael <mickael.ruzel@gmail.com>
2021-02-23 17:33:53 +0100

    Initial Commit

---
+++ UltimateTravelerTool_SwiftUI/Supporting Files/UltimateTravelerTool_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimateTravelerTool_SwiftUIApp.swift
+//  UltimateTravelerTool_SwiftUI
+//
+//  Created by mickael ruzel on 23/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct UltimateTravelerTool_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
